3a35e3409226d2cea0cbbe5b0ea6545d03ac4740
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-12-06 16:01:55 +0400

    Fixed push_head

--- AnDS-Lab2(med)/main.cpp
+++ AnDS-Lab2(med)/main.cpp
@@ -15,15 +15,15 @@ template<typename T>
 class LinkedList {
 public:
     LinkedList()
-        : head_(nullptr), tail_(nullptr) {}
+        : _head(nullptr), _tail(nullptr) {}
 
     explicit LinkedList(const LinkedList<T>& other)
-        : head_(nullptr), tail_(nullptr) {
+        : _head(nullptr), _tail(nullptr) {
         CopyFrom(other);
     }
 
     LinkedList(std::size_t count, int min_value, int max_value)
-        : head_(nullptr), tail_(nullptr) {
+        : _head(nullptr), _tail(nullptr) {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<T> dist(min_value, max_value);
@@ -38,7 +38,7 @@ public:
     }
 
     Node<T>* GetHead() const {
-        return head_;
+        return _head;
     }
 
     LinkedList<T>& operator=(const LinkedList<T>& other) {
@@ -49,68 +49,75 @@ public:
         return *this;
     }
 
-    void PushTail(const T& value) {
+    void push_tail(const T& value) {
         Node<T>* temp = new Node<T>(value);
         if (IsEmpty()) {
-            head_ = temp;
-            tail_ = temp;
+            _head = temp;
+            _tail = temp;
         }
         else {
-            tail_->next = temp;
-            tail_ = temp;
+            _tail->next = temp;
+            _tail = temp;
         }
     }
 
-    void PushTail(const LinkedList<T>& other) {
-        Node<T>* temp = other.head_;
+    void push_tail(const LinkedList<T>& other) {
+        Node<T>* temp = other._head;
         while (temp != nullptr) {
-            PushTail(temp->data);
+            push_tail(temp->data);
             temp = temp->next;
         }
     }
 
-    void PushHead(const T& value) {
+    void push_head(const T& value) {
         Node<T>* temp = new Node<T>(value);
         if (IsEmpty()) {
-            head_ = temp;
-            tail_ = temp;
+            _head = temp;
+            _tail = temp;
         }
         else {
-            temp->next = head_;
-            head_ = temp;
+            temp->next = _head;
+            _head = temp;
         }
     }
 
-    void PushHead(const LinkedList<T>& other) {
-        Node<T>* temp = other.head_;
+    void push_head(const LinkedList<T>& other) {
+        Node<T>* temp = other._head;
+        LinkedList<T> tempCopy; 
         while (temp != nullptr) {
-            PushHead(temp->data);
+            tempCopy.push_head(temp->data);
+            temp = temp->next;
+        }
+
+        temp = tempCopy._head;
+        while (temp != nullptr) {
+            push_head(temp->data);
             temp = temp->next;
         }
     }
 
-    void PopHead() {
+    void pop_head() {
         if (IsEmpty()) {
             throw std::runtime_error("List is empty.");
         }
-        Node<T>* temp = head_;
-        head_ = head_->next;
+        Node<T>* temp = _head;
+        _head = _head->next;
         delete temp;
     }
 
-    void PopTail() {
+    void pop_tail() {
         if (IsEmpty()) {
             throw std::runtime_error("List is empty.");
         }
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         Node<T>* prev = nullptr;
         while (current->next != nullptr) {
             prev = current;
             current = current->next;
         }
-        tail_ = prev;
-        if (tail_ != nullptr) {
-            tail_->next = nullptr;
+        _tail = prev;
+        if (_tail != nullptr) {
+            _tail->next = nullptr;
         }
         delete current;
     }
@@ -119,12 +126,12 @@ public:
         if (IsEmpty()) {
             throw std::runtime_error("List is empty.");
         }
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         Node<T>* prev = nullptr;
         while (current != nullptr) {
             if (current->data == value) {
-                if (current == head_) {
-                    head_ = head_->next;
+                if (current == _head) {
+                    _head = _head->next;
                 }
                 else {
                     prev->next = current->next;
@@ -141,7 +148,7 @@ public:
         if (IsEmpty()) {
             throw std::runtime_error("List is empty.");
         }
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         std::size_t iter = 0;
         while (current != nullptr && iter < index) {
             current = current->next;
@@ -157,7 +164,7 @@ public:
         if (IsEmpty()) {
             throw std::runtime_error("List is empty.");
         }
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         std::size_t iter = 0;
         while (current != nullptr && iter < index) {
             current = current->next;
@@ -170,23 +177,23 @@ public:
     }
 
     void Clear() {
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         while (current != nullptr) {
             Node<T>* node_to_delete = current;
             current = current->next;
             delete node_to_delete;
         }
-        head_ = nullptr;
-        tail_ = nullptr;
+        _head = nullptr;
+        _tail = nullptr;
     }
 
     bool IsEmpty() const {
-        return head_ == nullptr;
+        return _head == nullptr;
     }
 
     size_t Size() const {
         std::size_t size = 0;
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         while (current != nullptr) {
             size++;
             current = current->next;
@@ -199,7 +206,7 @@ public:
             return;
         }
 
-        Node<T>* current = head_;
+        Node<T>* current = _head;
         Node<T>* prev = nullptr;
         int count = 1;
 
@@ -208,8 +215,8 @@ public:
                 Node<T>* node_to_delete = current;
                 current = current->next;
 
-                if (node_to_delete == head_) {
-                    head_ = head_->next;
+                if (node_to_delete == _head) {
+                    _head = _head->next;
                 }
                 else {
                     prev->next = current;
@@ -227,7 +234,7 @@ public:
     }
 
     friend std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
-        Node<T>* current = list.head_;
+        Node<T>* current = list._head;
         while (current != nullptr) {
             os << current->data << " ";
             current = current->next;
@@ -237,7 +244,7 @@ public:
 
 private:
     void CopyFrom(const LinkedList<T>& other) {
-        Node<T>* temp = other.head_;
+        Node<T>* temp = other._head;
         while (temp != nullptr) {
             PushTail(temp->data);
             temp = temp->next;
@@ -245,15 +252,15 @@ private:
     }
 
 private:
-    Node<T>* head_;
-    Node<T>* tail_;
+    Node<T>* _head;
+    Node<T>* _tail;
 };
 
 void findUnluckyNumbersAndPrint(int N) {
     LinkedList<int> numbers;
 
     for (int i = 1; i <= N; ++i) {
-        numbers.PushTail(i);
+        numbers.push_tail(i);
     }
 
     int step = 2;
@@ -277,24 +284,38 @@ void findUnluckyNumbersAndPrint(int N) {
 int main() {
     try {
         LinkedList<int> list;
-        list.PushTail(1);
-        list.PushTail(2);
-        list.PushTail(3);
-        list.PushHead(0);
+        list.push_tail(1);
+        list.push_tail(2);
+        list.push_tail(3);
+        list.push_head(0);
 
         std::cout << "Original List: " << list << std::endl;
 
-        list.PopHead();
-        list.PopTail();
+        list.pop_head();
+        list.pop_tail();
 
         std::cout << "List after PopHead and PopTail: " << list << std::endl;
 
         LinkedList<int> anotherList;
-        anotherList.PushTail(4);
-        anotherList.PushTail(5);
-        anotherList.PushTail(6);
+        anotherList.push_tail(4);
+        anotherList.push_tail(5);
+        anotherList.push_tail(6);
 
-        list.PushTail(anotherList);
+        list.push_tail(anotherList);
+
+        LinkedList<int> list1;
+        list1.push_tail(11);
+        list1.push_tail(12);
+        list1.push_tail(13);
+        
+        LinkedList<int> anotherList1;
+        anotherList1.push_tail(1);
+        anotherList1.push_tail(2);
+        anotherList1.push_tail(3);
+
+        list1.push_head(anotherList1);
+
+        std::cout << "List1 after Pushhead with anotherList: " << list1 << std::endl;
 
         std::cout << "List after PushTail with anotherList: " << list << std::endl;
 
@@ -304,7 +325,7 @@ int main() {
 
         std::cout << "List after RemoveEveryNth(2): " << list << std::endl;
 
-        findUnluckyNumbersAndPrint(10);
+        findUnluckyNumbersAndPrint(100);
 
     }
     catch (const std::exception& e) {
